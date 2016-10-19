302b7bf44e9c9a336a46d5df556e6e22601312c2
Min <mingu08@berkeley.edu>
2016-10-19 23:38:51 +0000

    edited spec name from user to scheduler

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161019224817) do
+
+  create_table "schedulers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "login"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
