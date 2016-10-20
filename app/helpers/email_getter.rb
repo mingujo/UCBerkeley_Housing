a68a6976f1dd426bce157429dd5dadc7b20617b9
Abby Hersh <absterr08@berkeley.edu>
2016-10-20 05:56:42 +0000

    modified rspec testand added another email helper

---
+++ app/helpers/email_getter.rb
@@ -0,0 +1,11 @@
+class EmailGetter 
+
+    #takes a name and the hash emails, which is retreived from the Doc
+    def get_email(emails, name)
+        return emails[name]
+    end
+    
+end
+        
+    
+        
---
+++ spec/Helpers/email_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+require "spec_helper"
+
+
+RSpec.describe EmailGetter, :type => :helper do
+    
+    describe "get email" do
+        
+        before :each do 
+            #Calling get_email_mapping will return the emails hash
+            @emails = {"a" => "a@a.com"}
+        end
+            
+        it "gets email given name and hash" do  
+            email = EmailGetter.get_email(@emails, "a")
+            expect(email).to equal("a@a.com")
+        end
+    end
+end
