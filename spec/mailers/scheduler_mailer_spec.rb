2b7a2badfe648c964d641e763de968f7c4c63096
SoRa Bae <srbae1@berkeley.edu>
2016-10-20 01:49:03 +0000

    scheduler_mailer_spec rspec passed

--- spec/mailers/scheduler_mailer_spec.rb
+++ spec/mailers/scheduler_mailer_spec.rb
@@ -1,5 +1,28 @@
 require "rails_helper"
 
 RSpec.describe SchedulerMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+    describe "email_notification(guy_CA)" do
+        before :each do
+            @temp_guy = double("CA", :name=> "Sora", :email=>"testing@gmail.com")
+            @mail = SchedulerMailer.email_notification(@temp_guy)
+        end
+
+        it "expects email subject" do
+            expect(@mail.subject).to eq("Hey ____, You're scheduled on ______")
+        end
+        
+        it "sends from the default email" do
+            expect(@mail.from[0]).to eq("mingu08@berkeley.edu")
+        end
+        
+        it "sends to the given recipient" do
+            expect(@mail.to[0]).to eq(@temp_guy.email)
+        end
+
+        # it "expects the number of mails sent to increment by 1" do
+        #     expect(SchedulerMailer.deliver_now).to change { ActionMailer::Base.deliveries.count }.by(1)
+        # end
+    end
 end
+
+# expect {custom_mailer.deliver}.to change { ActionMailer::Base.deliveries.count }.by(1)
