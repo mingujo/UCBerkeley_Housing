aabccd2843cdab431d4eb25feaf5ef596959623c
SoRa Bae <srbae1@berkeley.edu>
2016-10-28 00:32:20 +0000

    step def for edit_ca

---
+++ features/step_definitions/edit_ca.rb
@@ -0,0 +1,20 @@
+Given /^I am a CA$/ do |ca|
+    pending
+end
+
+And /^the following CAs exist:$/ do |cas_table|
+    pending
+end
+
+And /^I click "([^"]*)"$/ do |button|
+    pending
+end
+
+And /^I click "([^"]*)" for the CA "([^"]*)"$/ do |button, name|
+    pending
+end
+
+
+
+
+
