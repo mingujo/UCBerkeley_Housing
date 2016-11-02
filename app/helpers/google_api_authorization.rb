9f03d6ca9fbc06022ba0204440d031c27120f9bf
Min <mingu08@berkeley.edu>
2016-11-02 20:38:58 +0000

    remove unnecessary code

--- app/helpers/google_api_authorization.rb
+++ app/helpers/google_api_authorization.rb
@@ -23,15 +23,15 @@ def authorize
   authorizer = Google::Auth::UserAuthorizer.new(client_id, SCOPE, token_store)
   user_id = 'default'
   credentials = authorizer.get_credentials(user_id)
-  if credentials.nil?
-    url = authorizer.get_authorization_url(base_url: OOB_URI)
-    puts "Open the following URL in the browser and enter the " +
-         "resulting code after authorization"
-    puts url
-    code = gets
-    credentials = authorizer.get_and_store_credentials_from_code(
-      user_id: user_id, code: code, base_url: OOB_URI)
-  end
+  # if credentials.nil?
+  #   url = authorizer.get_authorization_url(base_url: OOB_URI)
+  #   puts "Open the following URL in the browser and enter the " +
+  #       "resulting code after authorization"
+  #   puts url
+  #   code = gets
+  #   credentials = authorizer.get_and_store_credentials_from_code(
+  #     user_id: user_id, code: code, base_url: OOB_URI)
+  # end
   credentials
 end
 
