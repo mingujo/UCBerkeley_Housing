b000d291ef7ed7ef72afe020d9abcb012cc87814
Min <mingu08@berkeley.edu>
2016-11-03 05:20:17 +0000

    add rspec test for timeslot model add function

---
+++ spec/models/timeslot_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Timeslot, type: :model do
+	subject { described_class.new }
+	it "is valid with valid attributes" do
+		    subject.date = DateTime.now
+		    subject.time = "08:00"
+		    subject.ca_id = 1
+		    subject.client_name = "Anything"
+		    subject.phone_number = "Anything"
+		    subject.apt_number = "Anything"
+		    subject.current_tenant = "Anything"
+		    expect(subject).to be_valid
+	end
+  	
+end
