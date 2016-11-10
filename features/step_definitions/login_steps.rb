6ba46a29864d9a637aad6ddd02865f0f4df84b77
Erick Andres and Nathan Kim <e.andres4@yahoo.com>
2016-11-10 00:54:36 +0000

    login step definitions

---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,51 @@
+Given(/^"([^"]*)" is whitelisted$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should be in the login page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I sign in with "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I am signed in with "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click on sign out$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I sign in as an Admin$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I am signed in as an Admin$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I fill in email with "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I press submit$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I press delete$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see the confirmation prompt$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I press yes$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click sign out$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
