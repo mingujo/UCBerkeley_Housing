4f5d88a312befff194de0e4d6bc5f72a3f6b2a0d
SoRa Bae <srbae1@berkeley.edu>
2016-11-10 03:24:32 +0000

    added step def for ca_add_delete_avail

---
+++ features/step_definitions/ca_add_delete_avail.rb
@@ -0,0 +1,40 @@
+Given /^the following CAs exist:$/ do |cas_table|
+    pending
+end
+
+And /^the current month is "([^"]*)"$/ do |month|
+    pending
+end
+
+
+And /^"([^"]*)" has the following availability:$/ do |person|
+    pending
+end
+
+Given /^I am on the "([^"]*)" page for the CA "([^"]*)"$/ do |page_name, person|
+    pending
+end
+
+And I click /^"([^"]*)"$/ do |button|
+    pending
+end
+
+Then /^I should be on the "([^"]*)" page for the CA "([^"]*)"$/ do |page_name, person|
+    pending
+end
+
+Given /^I am on the "([^"]*)" page for the CA "([^"]*)"$/ do |page_name, person|
+    pending
+end
+
+And /^I press "([^"]*)" for the Availibility Slot "([^"]*)"$/ do |button, slot|
+    pending
+end
+
+Then /^I should not see availability for "([^"]*)"$/ do |arg| 
+    pending
+end
+
+And /^I should see availability for "([^"]*)"$/ do |arg| 
+    pending
+end
