7c74bf8b93f33a7baefb173751257b3a3da4ec48
Erick Andres and Nathan Kim <e.andres4@yahoo.com>
2016-11-17 03:39:08 +0000

    adding user_id column to CAs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161112020412) do
+ActiveRecord::Schema.define(version: 20161117033503) do
 
   create_table "cas", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20161112020412) do
     t.string   "phone_number"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "user_id"
   end
 
   create_table "timeslots", force: :cascade do |t|
