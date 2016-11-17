4b303f30b5e8bef69f3317fb930740313d2ebf29
bluemagic123 <nathankim96@gmail.com>
2016-11-17 03:58:29 +0000

    added the ability to login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,29 @@
 class SessionsController < ApplicationController
     def create
+        auth_hash = request.env['omniauth.auth']
+        email = auth_hash[:info][:email]
+        ca = Ca.find_by(email: email)
+        if ca
+            ca.user_id = auth_hash[:uid]
+            session[:user_id] = auth_hash[:uid]
+            ca.save
+            redirect_to '/'
+        elsif is_admin(email)
+            session[:user_id] = auth_hash[:uid]
+            redirect_to '/'
+        else
+            flash[:notice] = "This email is not authorized"
+            redirect_to '/'
+        end
+    end
+    
+    def is_admin(email)
+        return false
+    end
+    
+    def destroy
+        session[:user_id] = nil
+        flash[:notice] = 'Logout successful!'
         redirect_to '/'
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :cas
   get '/auth/:provider/callback', to: 'sessions#create'
+  get '/auth/logout', to: 'sessions#destroy'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
