8b3cc684019f3dbfdf1873b381f9952f7bc360b3
Min <mingu08@berkeley.edu>
2016-11-18 08:55:21 +0000

    delete migration file

--- db/migrate/20161118084424_fix_event_series_column_name.rb
+++
@@ -1,6 +0,0 @@
-class FixEventSeriesColumnName < ActiveRecord::Migration
-  def self.up
-    rename_column :event_series, :starttime, :start_time
-    rename_column :event_series, :endtime, :end_time
-  end
-end
