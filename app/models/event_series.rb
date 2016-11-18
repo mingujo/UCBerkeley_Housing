f786188ddec363363dff8064e16d9ee5ab70d12b
Min <mingu08@berkeley.edu>
2016-11-18 01:47:44 +0000

    comment out incomplete code

--- app/models/event_series.rb
+++ app/models/event_series.rb
@@ -24,42 +24,42 @@ class EventSeries < ActiveRecord::Base
 
   after_create :create_events_until_end_time
   
-  def create_events_until_end_time(end_time=RECURRING_EVENTS_UPTO)
-    st = start_time
-    et = end_time
-    p = r_period(period)
-    nst, net = st, et
-    while frequency.send(p).from_now(st) <= end_time
-      nst = st = frequency.send(p).from_now(st)
-      net = et = frequency.send(p).from_now(nst)
-      self.events.create(:title => title, :description => description, :all_day => all_day, :start_time => nst, :end_time => net)
-      Timeslot.create!(:starttime => event[:start_time],
-         :endtime => event[:end_time],
-         :ca_id => event[:ca_id])
+  # def create_events_until_end_time(end_time=RECURRING_EVENTS_UPTO)
+  #   st = start_time
+  #   et = end_time
+  #   p = r_period(period)
+  #   nst, net = st, et
+  #   while frequency.send(p).from_now(st) <= end_time
+  #     nst = st = frequency.send(p).from_now(st)
+  #     net = et = frequency.send(p).from_now(nst)
+  #     self.events.create(:title => title, :description => description, :all_day => all_day, :start_time => nst, :end_time => net)
+  #     Timeslot.create!(:starttime => event[:start_time],
+  #       :endtime => event[:end_time],
+  #       :ca_id => event[:ca_id])
       
-      if period.downcase == 'monthly' or period.downcase == 'yearly'
-        begin 
-          nst = DateTime.parse("#{start_time.hour}:#{start_time.min}:#{start_time.sec}, #{start_time.day}-#{st.month}-#{st.year}")  
-          net = DateTime.parse("#{end_time.hour}:#{end_time.min}:#{end_time.sec}, #{end_time.day}-#{et.month}-#{et.year}")
-        rescue
-          nst = net = nil
-        end
-      end
-    end
-  end
+  #     if period.downcase == 'monthly' or period.downcase == 'yearly'
+  #       begin 
+  #         nst = DateTime.parse("#{start_time.hour}:#{start_time.min}:#{start_time.sec}, #{start_time.day}-#{st.month}-#{st.year}")  
+  #         net = DateTime.parse("#{end_time.hour}:#{end_time.min}:#{end_time.sec}, #{end_time.day}-#{et.month}-#{et.year}")
+  #       rescue
+  #         nst = net = nil
+  #       end
+  #     end
+  #   end
+  # end
   
-  def r_period(period)
-    case period
-      when 'Daily'
-      p = 'days'
-      when "Weekly"
-      p = 'weeks'
-      when "Monthly"
-      p = 'months'
-      when "Yearly"
-      p = 'years'
-    end
-    return p
-  end
+  # def r_period(period)
+  #   case period
+  #     when 'Daily'
+  #     p = 'days'
+  #     when "Weekly"
+  #     p = 'weeks'
+  #     when "Monthly"
+  #     p = 'months'
+  #     when "Yearly"
+  #     p = 'years'
+  #   end
+  #   return p
+  # end
   
 end
