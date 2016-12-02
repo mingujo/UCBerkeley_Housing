d10e681b6dde9882b1bf8fbf1407094a127d08c4
Abby Hersh <absterr08@berkeley.edu>
2016-12-02 19:57:58 +0000

    added remove spreadsheet

--- app/controllers/spreadsheets_controller.rb
+++ app/controllers/spreadsheets_controller.rb
@@ -2,20 +2,35 @@ require_relative '../helpers/fetch_sheets.rb'
 
 class SpreadsheetsController < ApplicationController
     include SessionsHelper
-    before_action :require_login
-    before_action :require_admin_login, only: [:new, :create, :update, :destroy]
+    # before_action :require_login
+    # before_action :require_admin_login, only: [:new, :create, :update, :destroy]
+    
+    
     
     def index
         @spreadsheets = Spreadsheet.all
     end
     
-    def new #just renders view
-        
+    def new 
+        @spreadsheet = Spreadsheet.new
     end
     
+    
+    def get_date
+        year = params[:ca][:year].to_i
+        month = params[:ca][:month].to_i
+        date = Date.new(year, month, 1)
+        return date
+    end
+    
+
+    
     def create
-        spreadsheet_url = params[:ca][:spreadsheet_url]
-        spreadsheet_id = /\/spreadsheets\/d\/([a-zA-Z0-9\-_]+)/.match(spreadsheet_url) 
+        #byebug
+        date = get_date
+        
+        link = params[:ca][:spreadsheet_url]
+        spreadsheet_id = /\/spreadsheets\/d\/([a-zA-Z0-9\-_]+)/.match(link) 
         if !spreadsheet_id
             flash[:error] = "Invalid spreadsheet url"
             redirect_to new_spreadsheet_path
@@ -23,20 +38,27 @@ class SpreadsheetsController < ApplicationController
         end
         spreadsheet_id = spreadsheet_id[1]
         
-        days_in_month = params[:ca][:days_in_month].to_i
-        if days_in_month < 28 or days_in_month > 31
-            flash[:error] = "Invalid number of days in month"
-            redirect_to cas_generate_path
-            return
+        @spreadsheet = Spreadsheet.new({:month => date.month, :year => date.year, :spreadsheet_id => spreadsheet_id, :link => link}) 
+        if @spreadsheet.save
+            days_in_month = date.end_of_month.day
+            #populate_spreadsheet(days_in_month, spreadsheet_id, link)
+            flash[:success] = "Spreadsheet has been created"
+            redirect_to spreadsheets_path
         end
-      
-        if !validate_date(id)
-            flash[:error] = "Invalid date on spreadsheet. Please make sure the first day of the month is on the second row in the format of m/d/y weekday"
-            redirect_to new_spreadsheet_path
-            return
-        end
-      
-        populate_spreadsheet(days_in_month, spreadsheet_id, spreadsheet_url)
-        flash[:success] = "Spreadsheet has been created"
     end
+    
+    
+    def destroy
+        Spreadsheet.find(params[:id]).destroy
+        flash[:notice] = "Spreadsheet was removed from this app, but still exists."
+        redirect_to spreadsheets_path
+    end
+    
+    
+    private
+        def spreadsheet_params
+            params.require(:spreadsheet).permit('month', 'year', 'spreadsheet_id', 'link')
+        end
+        
+    
 end
