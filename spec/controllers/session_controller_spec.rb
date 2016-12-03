bcc43268991a071b19d5908226ccb6b10bebcf60
Erick Andres <e.andres4@yahoo.com>
2016-12-03 04:50:55 +0000

    rspec tests for session_controller

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -1,40 +1,30 @@
 require 'rails_helper'
 require 'time'
+require 'spec_helper'
 RSpec.describe SessionsController, type: :controller do
-#describe SessionsController do 
-  #describe "POST #create" do
-  #describe "DELETE #destroy" do how it looks like in events_controller
   before do
-    request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:github]
+    request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
   end
   
   describe "POST#create" do
- 
-    it "should successfully create a user" do
-      expect {
+    it "should successfully get a user" do
+      request.env["omniauth.auth"][:info][:email].should == 'housingnotificationsystem@gmail.com'
+    end
+      
+    describe "for signed in users" do
+      it "should successfully create a session" do
+        session[:user_id].should be_nil
         post :create, provider: :google_oauth2
-      }.to change{ Admin.count }.by(1)
+        session[:user_id].should_not be_nil
+      end
+      describe "for admin" do 
+        it "should direct the user to cas page" do
+          post :create, provider: :google_oauth2
+          response.should redirect_to '/cas'
+        end
+      end
     end
- 
-    it "should successfully create a session" do
-      session[:user_id].should be_nil
-      post :create, provider: :google_oauth2
-      session[:user_id].should_not be_nil
-    end
- 
-    it "should redirect the user to the root url" do
-      post :create, provider: :google_oauth2
-      response.should redirect_to root_url
-    end
- 
   end
-  
-  # was in cucumber steps for login
-  # auth_hash = {info: {email: email}, uid: email.hash}
-  # OmniAuth.config.test_mode = true
-  # OmniAuth.config.mock_auth[:google_oauth2] = auth_hash
-  
-  
   describe "DELETE#destroy" do
     before do
       post :create, provider: :google_oauth2
@@ -51,5 +41,11 @@ RSpec.describe SessionsController, type: :controller do
       flash[:notice].should eql('Logout successful!')
       response.should redirect_to '/'
     end
+  end
+end
+describe "for nonsigned users" do 
+  it 'should redirect to admin user' do
+    get :create, :provider => :google_oauth2
+    expect(response).to redirect_to('/auth/login')
   end
 end
